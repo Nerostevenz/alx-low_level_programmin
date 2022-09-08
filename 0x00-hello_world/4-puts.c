f41e350ffaad2d845fe1e1e4bf1aa4c9af615feb
tony <nerostevenz@gmail.com>
2022-09-08 14:22:34 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
+/**
+ * main- this is the entry and start of a function in c
+ *
+ * return- this goes back to the start of the function which is main 
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
