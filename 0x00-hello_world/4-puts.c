aa6f3f3e93327cec9c423bff971553a2ac29e051
tony <nerostevenz@gmail.com>
2022-09-08 14:28:03 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 /**
- * main- this is the entry and start of a function in c
+ * main - Entry point
  *
- * return- this goes back to the start of the function which is main 
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
