d9400cdf5a365b4b49c2cff3bd5051256c02335d
tony <nerostevenz@gmail.com>
2022-09-11 08:54:56 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print hexadecimals
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (x = 'a'; c < 'g'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n')
+
+	return (0)
+}
