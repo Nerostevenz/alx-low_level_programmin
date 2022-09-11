4bd0f8f5d4728d3f28ef3c3c5ba561ef1251dff5
tony <nerostevenz@gmail.com>
2022-09-11 07:16:56 -0700

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print some words
+ * Return: 0
+ */
+int main(void)
+{
+	char b = 'a';
+
+	while (b <= 'z')
+	{
+		if (b != 'q' && b != 'e')
+		putchar(c);
+		c++
+	}
+	putchar('\n');
+
+	return (0);
+}
