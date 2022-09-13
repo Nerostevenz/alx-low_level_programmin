9880c34e80ceeb9b199ee90c058dc15a5eb6edb2
tony <nerostevenz@gmail.com>
2022-09-13 09:03:31 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - main body
+ * Description: loweercase alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= z)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return(0);
+}
