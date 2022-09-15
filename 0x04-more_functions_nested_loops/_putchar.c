cf1ded779ae73bac8a7dcef4c490f479d193b165
tony <nerostevenz@gmail.com>
2022-09-15 09:43:30 -0700

    putchar

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+/**
+ * main - putchar character
+ * Return: Always 0
+ */
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,10 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int x, int y);
+void print_numbers(void);
+/**
+ * main - putchar for character
+ * Return: always 0
+ */
+int _putchar(char c);
+
