d09a953c7a7d5fcfac54cda1da73693c2812d5bc
tony <nerostevenz@gmail.com>
2022-09-19 10:03:39 -0700

    1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,15 +2,22 @@
 
 /**
  * swap_int - swap integers
+ *
  * @a: integer a
+ *
  * @b: integer b
+ *
  * Return: Always 0
  */
 
 void swap_int(int *a, int *b)
 {
+
 	int c;
+
 	c = *a;
+
 	*a = *b;
+
 	*b = c;
 }
