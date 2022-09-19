e117295117250e500736162a21899937c84a4e6c
tony <nerostevenz@gmail.com>
2022-09-19 09:26:37 -0700

    add

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap integers
+ * @a: integer a
+ * @b: integer b
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@
 #include <stdio.h>
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
