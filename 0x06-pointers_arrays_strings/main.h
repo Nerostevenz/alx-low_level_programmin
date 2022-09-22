e72a720648526df6b038583b597a0b32a7a57f34
tony <nerostevenz@gmail.com>
2022-09-22 10:57:46 -0700

    done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -8,4 +11,7 @@ char *cap_string(char *s);
 char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
+int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
+#endif
