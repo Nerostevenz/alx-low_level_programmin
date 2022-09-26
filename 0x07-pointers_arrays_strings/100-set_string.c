74247fefbf38381c4dc943c05ec35250a38a1b1b
tony <nerostevenz@gmail.com>
2022-09-26 08:08:27 -0700

    task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of a pointer
+ * @s: source address
+ * @to: target address
+ * Report: no return
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
