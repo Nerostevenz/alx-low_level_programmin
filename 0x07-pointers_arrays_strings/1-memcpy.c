f2ea5cf9ebe39c1e294d9b862a0c0d5ee3d8fa35
tony <nerostevenz@gmail.com>
2022-09-26 06:54:35 -0700

    edit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
-/**_memcpy - copy byte to address
+/**
+ * _memcpy -copy byte to addres
  * @dest: content to copy
  * @src: source data
  * @n: bytes to fill
  * Return: dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
 	unsigned int i;
-	
+
 	i = 0;
 	while (1 < n)
 	{
