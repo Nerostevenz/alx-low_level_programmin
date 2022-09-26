6d1d5ae56db4502632cecc2d1e49bb9e240ab32a
tony <nerostevenz@gmail.com>
2022-09-26 06:20:59 -0700

    task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**_memcpy - copy byte to address
+ * @dest: content to copy
+ * @src: source data
+ * @n: bytes to fill
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	
+	i = 0;
+	while (1 < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	return (dest);
+}
