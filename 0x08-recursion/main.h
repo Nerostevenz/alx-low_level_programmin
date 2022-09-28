dba5b20f3fe0f3e4e1e3a8e2caffaf4f9e53f7af
tony <nerostevenz@gmail.com>
2022-09-28 09:58:06 -0700

    task

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _evaluate(int i, int n);
+int _sqrt_recursion(int n);
+int evaluate_num(int num, int iterator);
+int is_prime_number(int num);
