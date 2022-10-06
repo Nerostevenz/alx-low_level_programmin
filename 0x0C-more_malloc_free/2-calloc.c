ad4923cbccc29c621973553dc4a9bb8523f5341d
tony <nerostevenz@gmail.com>
2022-10-06 13:45:51 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
  * _calloc -allocated memoria for nmeb elemn de zise bytes
  * @nmemb: number of element in the array
  * @size: bytes for each position in array
